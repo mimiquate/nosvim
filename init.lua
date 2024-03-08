62776d1d25b8d362bd4a73cd9d00fa91e6838df0
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-03-08 16:36:27 -0300

    refactor: package config in config callback

--- init.lua
+++ init.lua
@@ -12,34 +12,37 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "nvim-tree/nvim-tree.lua",
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      require("nvim-tree").setup({
+        filters = {
+          git_ignored = false,
+        },
+      })
+
+      vim.keymap.set("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
+    end
+  },
   "nvim-tree/nvim-web-devicons",
-  "nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true
+        },
+        endwise = {
+          enable = true
+        }
+      })
+    end
+  },
   "RRethy/nvim-treesitter-endwise"
-})
-
--- nvim-tree.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require("nvim-tree").setup({
-  filters = {
-    git_ignored = false,
-  },
-})
-
-vim.keymap.set("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
---
-
--- nvim-treesitter
-require('nvim-treesitter.configs').setup({
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true
-  },
-  endwise = {
-    enable = true
-  }
 })
