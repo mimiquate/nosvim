1406bf8d7086e6b213352b776630bf4a8f2cf424
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-03-13 15:26:17 -0300

    feat: ensures up to date basic lua, vim and vimdoc parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   main = "nvim-treesitter.configs",
   opts = {
+    ensure_installed = { "lua", "vim", "vimdoc" },
     highlight = {
       enable = true,
     },
